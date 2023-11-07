6fb1a77e27caae27c038a2fa84783d46da7f48e4
pedrocampos <pedro.campos@startaideia.com.br>
2023-11-07 01:20:02 -0300

    feat: final commit

--- router/routes.go
+++ router/routes.go
@@ -2,14 +2,18 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pedromartinsb/gopportunities/docs"
 	"github.com/pedromartinsb/gopportunities/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
-
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -17,4 +21,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
