e28fd6ed2a35d543d1ce2459e2fdc3774329f69f
pedrocampos <pedro.campos@startaideia.com.br>
2023-11-06 21:46:16 -0300

    feat: create handlers and configs

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/pedromartinsb/gopportunities/router"
+import (
+	"github.com/pedromartinsb/gopportunities/config"
+	"github.com/pedromartinsb/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
